6f249bc8925725b494d8660399444fc1b9615e0e
kumiho <konstantin.kim.mail@gmail.com>
2017-03-23 22:08:14 +0300

    fix

--- chapter 1/1.8.rkt
+++ chapter 1/1.8.rkt
@@ -1,19 +1,18 @@
 #lang racket
 (define (square x) (* x x))
+(define (cubic x) (* x x x))
 
 (define (sqrt-iter guess x)
   (if (good-enough? guess x)
       guess
       (sqrt-iter (improve guess x)
                  x)))
-(define (improve guess x)
-  (average x guess))
 
-(define (average x y)
-  (/ (+ (/ x (square y)) (* 2 y)) 3))
+(define (improve guess x)
+  (/ (+ (/ x (square guess)) (* 2 guess)) 3))
 
 (define (good-enough? guess x)
-  (< (percent-of x(abs (- (square guess) x))) 0.000001))
+  (< (percent-of x(abs (- (cubic guess) x))) 0.000001))
 
 (define (percent-of x y)
   (/ y (/ x 100.0) ))
