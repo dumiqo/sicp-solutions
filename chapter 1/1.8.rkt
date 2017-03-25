8de5c17cbd5f8cab64edc72a712f567e76ea638c
kumiho <konstantin.kim.mail@gmail.com>
2017-03-25 16:24:51 +0300

    fix 1.8 solution

--- chapter 1/1.8.rkt
+++ chapter 1/1.8.rkt
@@ -2,19 +2,19 @@
 (define (square x) (* x x))
 (define (cubic x) (* x x x))
 
-(define (sqrt-iter guess x)
+(define (cbr-iter guess x)
   (if (good-enough? guess x)
       guess
-      (sqrt-iter (improve guess x)
+      (cbr-iter (improve guess x)
                  x)))
 
-(define (improve guess x)
-  (/ (+ (/ x (square guess)) (* 2 guess)) 3))
+(define (improve  y x)
+  (/ (+ (/ x (square y)) (* 2 y)) 3))
 
 (define (good-enough? guess x)
-  (< (percent-of x(abs (- (cubic guess) x))) 0.000001))
+  (< (percent-of x (abs (- (cubic guess) x))) 1))
 
 (define (percent-of x y)
   (/ y (/ x 100.0) ))
 
-(define (sqr x) (sqrt-iter 1.0 x))
+(define (cbr x) (cbr-iter 1.0 x))
